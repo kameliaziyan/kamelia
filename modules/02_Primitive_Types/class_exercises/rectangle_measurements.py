ed3ac986f903899e395004adaae55cf751fc17a7
kamelia ziyan <kameliaziyan11gmail.com@Mac.lan>
2026-02-05 00:24:16 +0200

    finished project number 1

---
+++ modules/02_Primitive_Types/class_exercises/rectangle_measurements.py
@@ -0,0 +1,20 @@
+## Exercise 2:
+
+
+widthOfRectangle = input("enter the width of a rectangle: ")
+W = float(widthOfRectangle)
+heightOfRectangle = input("enter the height of a rectangle:")
+H = float(heightOfRectangle)
+
+areaOfTheRectangle = W * H
+perimeterOfTheRectangle = 2 * (W + H)
+diagonalOfTheRectangle = (W ** 2 + H ** 2) ** 0.5
+
+print("Width:", W )
+print("Height:", H )
+print("Area:", areaOfTheRectangle )
+print("Perimeter:", perimeterOfTheRectangle )
+print("diagonal:", diagonalOfTheRectangle )
+
+
+
---
+++ modules/02_Primitive_Types/class_exercises/temperature_converter.py
@@ -0,0 +1,15 @@
+
+## Exercise 1: 
+
+
+temperature = input("Enter temperature in Celsius: ")
+convertToFloat = float(temperature)
+convertToFahrenheit = ( convertToFloat * 9/5 ) + 32
+convertToKelvin = convertToFloat + 273.15
+
+
+print("Temperature in Celsius:", convertToFloat)
+print("Temperature in Fahrenheit:", convertToFahrenheit)
+print("Temperature in Kelvin:", convertToKelvin)
+
+
