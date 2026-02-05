db5aa95f057468acd8c37f23268b44dfc899a23b
Ilya Holtz <holtzilya2008@gmail.com>
2026-02-05 14:17:08 +0200

    upsated modules\03_common_ground\demos\solution\calculator.py

--- modules/03_common_ground/demos/solution/calculator.py
+++ modules/03_common_ground/demos/solution/calculator.py
@@ -1,8 +1,12 @@
-
-
 def add(a: int, b: int) -> int:
     return a + b
 
 
 def divide(a: int, b: int) -> float:
+    if (b == 0):
+        raise ZeroDivisionError()
     return a / b
+
+
+def multiply(a: int, b: int) -> int:
+    raise NotImplementedError()
